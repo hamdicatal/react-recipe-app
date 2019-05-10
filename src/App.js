4b9b3b5f909ee3e76f07a4049c94b4d281d6a366
Hamdi Çatal <hamdicatal@users.noreply.github.com>
2019-05-10 17:41:17 +0300

    Dosyalar oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api from './assets/API.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+
+    const APP_ID = api.APP_ID;
+    const APP_KEY = api.APP_KEY;
+
+  return(
+      <div className="App">
+       <h1>Recipe App</h1>;
+      </div>
   );
 }
 
